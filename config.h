370e66fbd5ff479ef7fab976c68b6b6625824890
Virashu <svladichek@gmail.com>
2022-12-17 10:18:40 +0300

    fu

--- config.h
+++ config.h
@@ -33,9 +33,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 5,       0,           -1 },
+	/* class      instance    title       tags mask     iscentred   isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            0,		1,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 5,       0,		0,           -1 },
 };
 
 /* layout(s) */
